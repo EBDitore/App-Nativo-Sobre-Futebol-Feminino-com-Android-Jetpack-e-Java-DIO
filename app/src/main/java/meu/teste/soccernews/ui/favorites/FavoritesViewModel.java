9d0b9771220e63503452ea16a2614403e701aa6b
Elielson <ebditore@gmail.com>
2022-08-28 13:54:01 -0300

    Rename completo

--- app/src/main/java/meu/teste/soccernews/ui/favorites/FavoritesViewModel.java
+++ app/src/main/java/meu/teste/soccernews/ui/favorites/FavoritesViewModel.java
@@ -1,16 +1,16 @@
-package meu.teste.soccernews.ui.home;
+package meu.teste.soccernews.ui.favorites;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class FavoritesViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public FavoritesViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("This is Favorites fragment");
     }
 
     public LiveData<String> getText() {
