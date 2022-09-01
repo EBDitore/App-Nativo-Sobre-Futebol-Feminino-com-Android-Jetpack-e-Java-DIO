f598137c81ac849b4a2747507d2d1be97bea29e9
Elielson <ebditore@gmail.com>
2022-09-01 00:05:29 -0300

    Funções de cliques adicionadas

--- app/src/main/java/meu/teste/soccernews/ui/adapters/NewsAdapter.java
+++ app/src/main/java/meu/teste/soccernews/ui/adapters/NewsAdapter.java
@@ -3,6 +3,7 @@ package meu.teste.soccernews.ui.adapters;
 import android.content.Intent;
 import android.net.Uri;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -18,9 +19,11 @@ import meu.teste.soccernews.domain.News;
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
 
     private List<News> news;
+    private View.OnClickListener favoriteListener; // Adiciona o OnClickListener para ativar a função do botão de favoritar
 
-    public NewsAdapter(List<News> news) {
+    public NewsAdapter(List<News> news, View.OnClickListener favoriteListener) { // Adiciona o OnClickListener para ativar a função do botão de favoritar
         this.news = news;
+        this.favoriteListener = favoriteListener;
     }
 
     @NonNull
@@ -37,12 +40,22 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
         holder.binding.tvTitle.setText(news.getTitle());
         holder.binding.tvDescription.setText(news.getDescription());
         Picasso.get().load(news.getImage()).fit().into(holder.binding.ivThumbnail); // Atribui ao imageview a imagem da api
-        holder.binding.btOpenLink.setOnClickListener(view -> {
+
+        holder.binding.btOpenLink.setOnClickListener(view -> { // Abre o link
             String url = "http://www.example.com";
             Intent i = new Intent(Intent.ACTION_VIEW);
             i.setData(Uri.parse(news.getLink()));
             holder.itemView.getContext().startActivity(i); // Pega o context para o startActivity
         });
+
+        holder.binding.ivShare.setOnClickListener(view -> { // Compartilha o link
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.setType("text/plain");
+            i.putExtra(Intent.EXTRA_TEXT, news.getLink());
+            holder.itemView.getContext().startActivity(Intent.createChooser(i, ""));
+        });
+
+        holder.binding.ivFavorite.setOnClickListener(this.favoriteListener); // Salva o clicado no banco de dados (O evento é instanciado pelo fragmento)
     }
 
     @Override
--- app/src/main/java/meu/teste/soccernews/ui/news/NewsFragment.java
+++ app/src/main/java/meu/teste/soccernews/ui/news/NewsFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -18,18 +17,17 @@ public class NewsFragment extends Fragment {
 
     private FragmentNewsBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        NewsViewModel newsViewModel =
-                new ViewModelProvider(this).get(NewsViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        NewsViewModel newsViewModel = new ViewModelProvider(this).get(NewsViewModel.class);
 
         binding = FragmentNewsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View roFot = binding.getRoot();
 
         binding.rvNews.setLayoutManager(new LinearLayoutManager(getContext())); // Atribui ao Recyclerview um linear layout
-        final TextView textView = binding.textNews;
         newsViewModel.getNews().observe(getViewLifecycleOwner(), news -> {
-            binding.rvNews.setAdapter(new NewsAdapter(news)); // Atribui o Adapter ao recyclerview
+            binding.rvNews.setAdapter(new NewsAdapter(news, view -> { // O view é chamado quando o botão ivFavorite de NewsAdapter é clicado
+
+            })); // Atribui o Adapter ao recyclerview
         });
         return root;
     }
